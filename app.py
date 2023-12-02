b80225d3cebc1dce5f39df3b2e7fdea06c9826d8
timli6307 <timli6307@gmail.com>
2023-12-03 02:48:24 +0800

    first commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     lib.signIn(account, password)
     connection = lib.create_SQL_database()
 
+
     while True:
         choice = input("請輸入您的選擇 [0-7]: ")
         if choice == '0':
